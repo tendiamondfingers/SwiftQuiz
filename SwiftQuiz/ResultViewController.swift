cca54ff62caf8ef65893cf54493e54843a03328b
Stanislav Sazonov <slavk11@yandex.ru>
2023-04-27 21:31:34 +0300

    Created ResultViewController

---
+++ SwiftQuiz/ResultViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ResultViewController.swift
+//  SwiftQuiz
+//
+//  Created by Сазонов Станислав on 27.04.2023.
+//
+
+import UIKit
+
+final class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
