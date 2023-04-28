1eb80e1785c98454f00c8200ac48a07ef91806ce
DmKeller <dmit.keller@gmail.com>
2023-04-28 23:11:46 +0300

    UI

---
+++ SwiftQuiz/QuizViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftQuiz
+//
+//  Created by Сергей Захаров on 26.04.2023.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
