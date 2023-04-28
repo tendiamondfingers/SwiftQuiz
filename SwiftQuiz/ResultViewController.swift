b7118ae27049cf1a12bccef94816445e056d7b71
Stanislav Sazonov <slavk11@yandex.ru>
2023-04-28 19:32:06 +0300

    Final ResultViewController

--- SwiftQuiz/ResultViewController.swift
+++ SwiftQuiz/ResultViewController.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 final class ResultViewController: UIViewController {
-
+    
+    @IBOutlet var userScoreLabel: UILabel!
+    
+    var result = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        userScoreLabel.text = "Твой результат \(result)"
     }
-
-
+    
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        navigationController?.popToRootViewController(animated: true)
+        }
+    
 }
