f765b75b52701a244a05968f0b78d3c2217afade
Stanislav Sazonov <slavk11@yandex.ru>
2023-04-29 19:41:02 +0300

    Скруглил кнопку "Играть"

--- SwiftQuiz/QuizViewController.swift
+++ SwiftQuiz/QuizViewController.swift
@@ -12,6 +12,11 @@ class QuizViewController: UIViewController {
     var team: [Team]!
     var questions: [Question]!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //playButton.layer.cornerRadius = 15
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let navigationVC = segue.destination as? UINavigationController else {
             return
