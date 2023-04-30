52ea63b35e39a6e8c3c2ff242b4f1d51c70d29b8
Stanislav Sazonov <slavk11@yandex.ru>
2023-04-30 12:25:11 +0300

    Добавил title при возврате на экран с темами

--- SwiftQuiz/ResultViewController.swift
+++ SwiftQuiz/ResultViewController.swift
@@ -22,8 +22,9 @@ final class ResultViewController: UIViewController {
     
     @IBAction func doneButtonPressed() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-         let topicVC = storyboard.instantiateViewController(identifier: "TopicTableViewController") as! TopicTableViewController
-        topicVC.modalPresentationStyle = .fullScreen
-         present(topicVC, animated: true)
+        let topicVC = storyboard.instantiateViewController(identifier: "TopicTableViewController") as! TopicTableViewController
+        let navigationController = UINavigationController(rootViewController: topicVC)
+        navigationController.modalPresentationStyle = .fullScreen
+        present(navigationController, animated: true)
     }
 }
